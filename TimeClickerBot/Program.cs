426ec004a372f8f4ea275becbf8176745755abb8
Tobias Savinainen <tobias@savinainen.se>
2015-08-05 00:23:21 +0200

    Prevent multiple instances

--- TimeClickerBot/Program.cs
+++ TimeClickerBot/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,6 +16,14 @@ namespace TimeClickerBot
         [STAThread]
         static void Main()
         {
+            var processName = Process.GetCurrentProcess().ProcessName;
+
+            if (Process.GetProcesses().Count(x => x.ProcessName == processName) > 1)
+            {
+                MessageBox.Show("TimeClickerBot is already running", "TimeClickerBot - By Tobias Savinainen", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TimeClickerForm());
